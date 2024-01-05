3d60acf5187880c618c53bc132b0a3993dac6ae3
Asmita Sagarkar <asmitasagarkar@gmail.com>
2024-01-06 00:55:57 +0530

    Card compoenent updated

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -3,34 +3,44 @@ import {Link} from "react-router-dom"
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LaunchIcon from '@mui/icons-material/Launch';
 import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+import { useCallback, useState } from "react";
+import CancelIcon from '@mui/icons-material/Cancel';
+import { debounce } from "lodash";
 
 export default function Card({details}){
+
+    const [onMouseHover,setOnMouseHover] = useState(false);
+    const [onShow,setOnShow]=useState(false);
+    const debounceState = useCallback(debounce(setOnMouseHover,2000),[]);
+    
     return (
 
         <div class="projectgrid">
             <div class="projectproject-items">
-                <div class="projectimg">
-                    <img src={details.background} alt="Loading" />
+            <div class="projectimg" 
+                onMouseEnter={()=>debounceState(true)}
+                onMouseLeave={()=>debounceState(false)}
+                
+                
+                >
+                    <img src={details.background} alt="Loading" className="bgimage" />
                 </div>
-                <div class="projecthover-item">
+
+                {onMouseHover && (<div class="projecthover-item fade-enter fade-enter-active">
                     <h3>Project Source</h3>
-
                     <div class="projecticons">
-                        <Link className="fa-brands fa-github" to={details.github}>{<GitHubIcon />}</Link>
-                        <Link className="fa-solid fa-paperclip" to={details.link}>{<LaunchIcon />}</Link>
-                        <button className="fa-solid fa-lightbulb">{<TipsAndUpdatesIcon />}</button>
-                        
-                        
-                    </div>
+                        <Link className="projecticon" to={details.github}>{<GitHubIcon />}</Link>
+                        <Link className="projecticon" to={details.link}>{<LaunchIcon />}</Link>
+                        <button className="projecticon" onClick={()=>setOnShow(true)}>{<TipsAndUpdatesIcon />}</button>
+                    </div>  
+                </div>)}
 
-
-                    <div class="projectpmini projectmini" id="pmini">
+                {onShow && (<div class="projectpmini projectmini" id="pmini">
                         <h3>{details.heading}</h3>
                         <p>{details.para}</p>
-                        <i class="fa-solid fa-circle-xmark close" id="pclose"></i>
+                        <CancelIcon onClick={()=>setOnShow(false)} />
 
-                    </div>
-                </div>
+                </div>)}
             </div>
         </div>
     )
